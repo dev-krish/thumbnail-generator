845e1b7787a851c371daf7644ce89e78f8715d00
Krish <krishnendu.dutta.dev@gmail.com>
2026-08-12 19:58:02 +0530

    Refactor code structure for improved readability and maintainability

--- backend/config.py
+++ backend/config.py
@@ -8,5 +8,6 @@ OPEN_API_KEY = os.getenv("OPEN_API_KEY","")
 IMAGEKIT_PRIVATE_KEY = os.getenv("IMAGEKIT_PRIVATE_KEY","")
 IMAGEKIT_PUBLIC_KEY = os.getenv("IMAGEKIT_PUBLIC_KEY","")
 IMAGEKIT_URL_ENDPOINT = os.getenv("IMAGEKIT_URL_ENDPOINT","")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY","")
 
 DATABASE_URL = "sqlite:///./thumbnailbuilder.db"
